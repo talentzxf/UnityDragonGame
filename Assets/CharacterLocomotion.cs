c1c32f4f5222bc04a0f68581f773dc7841ff283f
VincentZhang <talentzxf@hotmail.com>
2023-12-10 18:30:16 +0800

    Added more animations

--- Assets/CharacterLocomotion.cs
+++ Assets/CharacterLocomotion.cs
@@ -7,9 +7,22 @@ public class CharacterLocomotion : MonoBehaviour
     [SerializeField] private float maxVelocity = 5f;
 
     private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private int _isIdleHash = Animator.StringToHash("isIdle");
+    private int _speed = Animator.StringToHash("speed");
+    
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+
+        if (_animator == null)
+        {
+            Debug.LogError("Can't find animator!!!");
+        }
+
+        _animator.applyRootMotion = false;
     }
 
     
@@ -18,11 +31,26 @@ public class CharacterLocomotion : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
-        float maxVelocityChange = Time.deltaTime * maxVelocity;
-        
-        Vector3 forwardVelocity = vertical * maxVelocityChange * transform.forward;
-        Vector3 horizontalVelocity = horizontal * maxVelocityChange * transform.right;
+        Vector3 forwardVelocity = vertical * maxVelocity * transform.forward;
+        Vector3 horizontalVelocity = horizontal * maxVelocity * transform.right;
 
-        _rigidbody.MovePosition(_rigidbody.position + forwardVelocity + horizontalVelocity);
+        Vector3 resultVelocity = forwardVelocity + horizontalVelocity;
+        _rigidbody.MovePosition(_rigidbody.position + resultVelocity * Time.deltaTime);
+
+        float speed = resultVelocity.magnitude;
+        Debug.Log("Speed:" + speed);
+        if ( speed < Mathf.Epsilon)
+        {
+            _animator.SetBool(_isIdleHash, true);
+            
+            Debug.Log("IsIdle: true");
+        }
+        else
+        {
+            _animator.SetBool(_isIdleHash, false);
+            Debug.Log("IsIdle: false");
+        }
+        
+        _animator.SetFloat(_speed, speed);
     }
 }
