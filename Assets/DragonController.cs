e3ff15e9c7f38de6262bf485e82eba09e2224e1c
talentzxf <talentzxf@hotmail.com>
2023-12-11 19:27:58 +0800

    Added Dragon Controller

--- Assets/DragonController.cs
+++ Assets/DragonController.cs
@@ -7,19 +7,7 @@ public class DragonController : MonoBehaviour
 
     private Animator _animator;
     
-    int IdleSimple = Animator.StringToHash("IdleSimple");
-    int IdleAgressive = Animator.StringToHash("IdleAgressive");
-    int IdleRestless = Animator.StringToHash("IdleRestless");
-    int Walk = Animator.StringToHash("Walk");
-    int BattleStance = Animator.StringToHash("BattleStance");
-    int Bite = Animator.StringToHash("Bite");
-    int Drakaris = Animator.StringToHash("Drakaris");
-    int FlyingFWD = Animator.StringToHash("FlyingFWD");
-    int FlyingAttack = Animator.StringToHash("FlyingAttack");
-    int Hover = Animator.StringToHash("Hover");
-    int Lands = Animator.StringToHash("Lands");
-    int TakeOff = Animator.StringToHash("TakeOff");
-    int Die = Animator.StringToHash("Die");
+    int takeOff = Animator.StringToHash("TakeOff");
     
     void Start()
     {
@@ -38,19 +26,7 @@ public class DragonController : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            _animator.SetBool(IdleSimple, true);
-            _animator.SetBool(IdleAgressive, false);
-            _animator.SetBool(IdleRestless, false);
-            _animator.SetBool(Walk, false);
-            _animator.SetBool(BattleStance, false);
-            _animator.SetBool(Bite, false);
-            _animator.SetBool(Drakaris, false);
-            _animator.SetBool(FlyingFWD, false);
-            _animator.SetBool(FlyingAttack, false);
-            _animator.SetBool(Hover, false);
-            _animator.SetBool(Lands, false);
-            _animator.SetBool(TakeOff, false);
-            _animator.SetBool(Die, false);
+            _animator.SetTrigger(takeOff);
         }
     }
 }
