f8ffec1224794cf251f9555106a1578a4a6426db
talentzxf <talentzxf@hotmail.com>
2023-12-13 17:14:26 +0800

    Can fly can control, but why flying higher and higher?

--- Assets/VZLocomotion/DragonProcessors/DragonFlying.cs
+++ Assets/VZLocomotion/DragonProcessors/DragonFlying.cs
@@ -6,11 +6,11 @@ namespace VZLocomotion.DragonProcessors
     {
         private Rigidbody _rigidbody;
 
-        private float maxSpeed = 5.0f;
-        private float rotationVelocity = 100f;
-        
+        private float maxSpeed = 500.0f;
+        private float rotationSpeed = 1000f;
+
         private int speedFWD = Animator.StringToHash("SpeedFWD");
-        
+
         public override void OnActive(params object[] parameters)
         {
             base.OnActive(parameters);
@@ -20,22 +20,22 @@ namespace VZLocomotion.DragonProcessors
 
         public override void Update()
         {
-            float vertical = Input.GetAxis("Vertical");
-            Vector3 forwardVelocity = vertical * Time.deltaTime * maxSpeed * _transform.forward;
-            _rigidbody.AddForce(forwardVelocity, ForceMode.VelocityChange);
-
             float horizontal = Input.GetAxis("Horizontal");
-            if (horizontal != 0)
-            {
-                float rotationAmount = rotationVelocity * horizontal * Time.deltaTime;
-                Quaternion deltaRotation = Quaternion.Euler(_transform.up * rotationAmount);
-                _rigidbody.MoveRotation(_rigidbody.rotation * deltaRotation);
+            float vertical = Input.GetAxis("Vertical");
+            Vector3 force = (vertical * _transform.forward + horizontal * _transform.right).normalized *
+                            Time.deltaTime * maxSpeed;
 
-                Vector3 velocity = _rigidbody.velocity;
-                velocity = Quaternion.Euler(rotationAmount * Time.deltaTime * _transform.up) * velocity;
-                _rigidbody.velocity = velocity.normalized * maxSpeed;
-            }
-            
+            _rigidbody.AddForce(force, ForceMode.Acceleration);
+
+            Vector3 targetDirection = _rigidbody.velocity.normalized;
+
+            Vector3 currentDirection = _transform.forward;
+            Vector3 newDirection = Vector3.RotateTowards(currentDirection, targetDirection,
+                rotationSpeed * Time.fixedDeltaTime, 0.0f);
+            Quaternion newRotation = Quaternion.LookRotation(newDirection);
+
+            _rigidbody.MoveRotation(newRotation);
+
             _animator.SetFloat(speedFWD, _rigidbody.velocity.magnitude);
         }
     }
