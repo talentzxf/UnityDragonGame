91db7fd403a06eb83ee4b3472a08be3467739883
VincentZhang <talentzxf@hotmail.com>
2023-12-16 22:14:54 +0800

    Continue understanding Photon network

--- Assets/LearnPhotonNetwork/BasicSpawner.cs
+++ Assets/LearnPhotonNetwork/BasicSpawner.cs
@@ -83,6 +83,13 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
         }
     }
 
+    private bool _mouseButton0;
+
+    private void Update()
+    {
+        _mouseButton0 = _mouseButton0 | Input.GetMouseButton(0);
+    }
+
     public void OnInput(NetworkRunner runner, NetworkInput input)
     {
         var data = new NetworkInputData();
@@ -96,6 +103,9 @@ public class BasicSpawner : MonoBehaviour, INetworkRunnerCallbacks
 
         if (Input.GetKey(KeyCode.D))
             data.direction += Vector3.right;
+        
+        data.buttons.Set(NetworkInputData.MOUSEBUTTON0, _mouseButton0);
+        _mouseButton0 = false;
 
         input.Set(data);
     }
--- Assets/LearnPhotonNetwork/Player.cs
+++ Assets/LearnPhotonNetwork/Player.cs
@@ -1,5 +1,5 @@
-using System;
 using Fusion;
+using UnityEngine;
 
 public class Player : NetworkBehaviour
 {
@@ -10,12 +10,31 @@ public class Player : NetworkBehaviour
         _cc = GetComponent<NetworkCharacterController>();
     }
 
+    [SerializeField] private Ball _prefabBall;
+    private Vector3 _forward = Vector3.forward;
+
+    [Networked] private TickTimer delay { get; set; }
+
     public override void FixedUpdateNetwork()
     {
         if (GetInput(out NetworkInputData data))
         {
             data.direction.Normalize();
             _cc.Move(5 * data.direction * Runner.DeltaTime);
+
+            if (data.direction.sqrMagnitude > 0)
+                _forward = data.direction;
+
+            if (HasStateAuthority && delay.ExpiredOrNotRunning(Runner))
+            {
+                if (data.buttons.IsSet(NetworkInputData.MOUSEBUTTON0))
+                {
+                    delay = TickTimer.CreateFromSeconds(Runner, 0.5f);
+                    Runner.Spawn(_prefabBall, transform.position + _forward,
+                        Quaternion.LookRotation(_forward), Object.InputAuthority,
+                        (runner, o) => { o.GetComponent<Ball>().Init(); });
+                }
+            }
         }
     }
 }
