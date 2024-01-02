9be2fc26f06bd66725b02fdbc2f6936cce854843
VincentZhang <talentzxf@hotmail.com>
2024-01-02 22:35:41 +0800

    Added LineGenerator

--- Assets/DragonGameNetworkProject/Gems/LineGemGenerator.cs
+++ Assets/DragonGameNetworkProject/Gems/LineGemGenerator.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class LineGemGenerator : MonoBehaviour
+public class LineGemGenerator : AbstractGemGenerator
 {
-    // Start is called before the first frame update
-    void Start()
+    [CustomEditor(typeof(LineGemGenerator))]
+    class LineGemGeneratorEditor: GemGeneratorEditor{}
+
+    [SerializeField] private float length = 5.0f;
+    [SerializeField] private float interval = 1.0f;
+    [SerializeField] private Vector3 dir = Vector3.forward;
+
+    public override void GenerateGems()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    private Vector3 endPoint = Vector3.forward; // 初始方向设为Z轴方向
+    private bool isDragging = false;
+    
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.blue;
+        // 绘制可调整方向的线条
+        Gizmos.DrawLine(transform.position, transform.position + endPoint);
+
+        // 检测鼠标事件
+        Event currentEvent = Event.current;
+        if (currentEvent.type == EventType.MouseDown && currentEvent.button == 0)
+        {
+            Ray ray = HandleUtility.GUIPointToWorldRay(currentEvent.mousePosition);
+            Plane plane = new Plane(transform.forward, transform.position);
+
+            float distance;
+            if (plane.Raycast(ray, out distance))
+            {
+                endPoint = ray.GetPoint(distance) - transform.position;
+                endPoint = Vector3.ProjectOnPlane(endPoint, transform.forward).normalized;
+            }
+        }
     }
 }
--- Assets/DragonGameNetworkProject/Gems/RandomGemGenerator.cs
+++ Assets/DragonGameNetworkProject/Gems/RandomGemGenerator.cs
@@ -1,49 +1,85 @@
 using System;
-using System.Collections.Generic;
-using Fusion;
+using System.Collections.Generic;using DragonGameNetworkProject;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class RandomGemGenerator : NetworkBehaviour
+public abstract class AbstractGemGenerator : MonoBehaviour
 {
-    [SerializeField] private float maxRange = 5.0f;
-    [SerializeField] private List<GameObject> gems;
+    [SerializeField] protected List<GameObject> gems;
+
+    public abstract void GenerateGems();
+    
+    public void DeleteAllGems()
+    {
+        while (transform.childCount != 0)
+        {
+            for(int childIdx = 0 ; childIdx < transform.childCount; childIdx++)
+            {
+                Undo.DestroyObjectImmediate(transform.GetChild(childIdx).gameObject);
+            }            
+        }
+    }
+}
+
+public class GemGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        AbstractGemGenerator gemGenerator = target as AbstractGemGenerator;
+        if (GUILayout.Button("Generate"))
+        {
+            gemGenerator.GenerateGems();
+        }
+
+        if (GUILayout.Button("Delete All"))
+        {
+            gemGenerator.DeleteAllGems();
+        }
+    }
+}
+
+public class RandomGemGenerator : AbstractGemGenerator
+{
+#if UNITY_EDITOR
+    [SerializeField] private float radius = 5.0f;
 
     [SerializeField] private int totalCount = 5;
+
+    [CustomEditor(typeof(RandomGemGenerator))]
+    class RandomGemGeneratorEditor: GemGeneratorEditor{}
+
+    public override void GenerateGems()
+    {
+        for (int i = 0; i < totalCount; i++)
+        {
+            int randomIdx = Random.Range(0, gems.Count);
+            GameObject gemPrefab = gems[randomIdx];
+            Vector3 randomPosition = Random.insideUnitSphere * radius;
+            Vector3 position = transform.position + randomPosition;
+            GameObject gem = null;
+            try
+            {
+                gem = Instantiate(gemPrefab, position, gemPrefab.transform.rotation);
+            }
+            catch (Exception)
+            {
+                Debug.LogError("Can't spawn the Gem!");
+            }
+        
+            if (gem != null)
+            {
+                gem.transform.parent = transform;
+            }
+        }
+    }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.blue;
-        Gizmos.DrawWireSphere(transform.position, maxRange);
+        Gizmos.DrawWireSphere(transform.position, radius);
     }
-
-    public override void Spawned()
-    {
-        base.Spawned();
-
-        if (Runner.IsSharedModeMasterClient)
-        {
-            for (int i = 0; i < totalCount; i++)
-            {
-                int randomIdx = Random.Range(0, gems.Count);
-                GameObject gemPrefab = gems[randomIdx];
-                Vector3 randomPosition = Random.insideUnitSphere * maxRange;
-                Vector3 position = transform.position + randomPosition;
-                NetworkObject gem = null;
-                try
-                {
-                   gem = Runner.Spawn(gemPrefab, position);
-                }
-                catch (Exception)
-                {
-                    Debug.LogError("Can't spawn the Gem!");
-                }
-
-                if (gem != null)
-                {
-                    gem.transform.parent = transform;
-                }
-            }
-        }
-    }
+    
+#endif
 }
