ebd1b7c3534a29f15ffdd4d59b383a15917c3cb5
talentzxf <talentzxf@hotmail.com>
2024-01-09 19:25:39 +0800

    Why charactercontroller simple move is not working as expected?

--- Assets/DragonGameNetworkProject/DragonAvatarMovements/DragonAvatarGroundMovement.cs
+++ Assets/DragonGameNetworkProject/DragonAvatarMovements/DragonAvatarGroundMovement.cs
@@ -8,10 +8,10 @@ namespace DragonGameNetworkProject.DragonAvatarMovements
 
         public float rotationSpeed = 10.0f;
 
-        public float walkSpeed = 2.0f;
+        public float walkSpeed = 10f;
 
         private Camera camera;
-        
+
         private int _isWalkingHash = Animator.StringToHash("IsWalking");
 
         public override void Spawned()
@@ -42,25 +42,28 @@ namespace DragonGameNetworkProject.DragonAvatarMovements
                 networkAnimator.Animator.SetBool(_isWalkingHash, false);
                 return;
             }
+            
+            networkAnimator.Animator.SetBool(_isWalkingHash, true);
+            
+            Transform camTransform = camera.transform;
 
-            Vector3 forwardVelocity = vertical * camera.transform.forward;
-            Vector3 horizontalVelocity = horizontal * camera.transform.right;
+            Vector3 forwardVelocity = vertical * camTransform.forward;
+            Vector3 horizontalVelocity = horizontal * camTransform.right;
 
-            Vector3 resultVelocityDir = forwardVelocity + horizontalVelocity;
+            Vector3 addedVelocity = forwardVelocity + horizontalVelocity;
+            addedVelocity.y = 0.0f; // No need to care about y axis.
+            Vector3 resultVelocityDir = addedVelocity.normalized;
+            
+            // Lerp rotate the character.
+            Quaternion targetRotation = Quaternion.LookRotation(resultVelocityDir, Vector3.up);
+            Quaternion resultRotation =
+                Quaternion.Slerp(ccTransform.rotation, targetRotation, rotationSpeed * Runner.DeltaTime);
+            // Keep the character always up.
+            resultRotation.x = 0;
+            resultRotation.z = 0;
 
-               networkAnimator.Animator.SetBool(_isWalkingHash, true);
-
-                // Lerp rotate the character.
-                Quaternion targetRotation = Quaternion.LookRotation(resultVelocityDir, Vector3.up);
-                Quaternion resultRotation =
-                    Quaternion.Slerp(ccTransform.rotation, targetRotation, rotationSpeed * Runner.DeltaTime);
-
-                // Keep the character always up.
-                resultRotation.x = 0;
-                resultRotation.z = 0;
-                ccTransform.rotation = resultRotation;
-                
-                cc.SimpleMove(resultVelocityDir.normalized * walkSpeed);
+            ccTransform.rotation = resultRotation;
+            cc.SimpleMove(resultVelocityDir * walkSpeed);
         }
     }
 }
