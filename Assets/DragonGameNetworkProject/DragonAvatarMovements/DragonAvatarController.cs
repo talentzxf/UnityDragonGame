9dd9ecdc9de896b89ffe9d6c3a72ddcd3ae7ff7c
talentzxf <talentzxf@hotmail.com>
2024-01-11 13:17:18 +0800

    Setup colliders

--- Assets/DragonGameNetworkProject/DragonAvatarMovements/DragonAvatarController.cs
+++ Assets/DragonGameNetworkProject/DragonAvatarMovements/DragonAvatarController.cs
@@ -1,7 +1,31 @@
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+
 namespace DragonGameNetworkProject.DragonAvatarMovements
 {
+
+    
     public class DragonAvatarController: CharacterMovementController
     {
-        
+#if UNITY_EDITOR
+        [CustomEditor(typeof(DragonAvatarController))]
+        public class DragonAvatarControllerEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                base.OnInspectorGUI();
+                DragonAvatarController dragonAvatarController = target as DragonAvatarController;
+                if (GUILayout.Button("Setup All Colliders"))
+                {
+                    Collider[] colliders = dragonAvatarController.GetComponentsInChildren<Collider>(true);
+                    foreach (var collider in colliders)
+                    {
+                        collider.gameObject.tag = "Player";
+                    }
+                }
+            }
+        }
+#endif
     }
 }
